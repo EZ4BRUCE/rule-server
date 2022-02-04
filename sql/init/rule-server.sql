0adda2e3b57cc0cc597f97a63b2491bdcd0479ce
EZ4BRUCE <lhy122786302@gmail.com>
2022-02-04 17:23:20 +0800

    添加磁盘的默认聚合规则

--- sql/init/rule-server.sql
+++ sql/init/rule-server.sql
@@ -42,7 +42,11 @@ INSERT INTO `aggregator` VALUES (21, 'CPU占用最大值监测', 'cpu_rate', 1, 
 INSERT INTO `aggregator` VALUES (22, 'CPU占用平均值监测', 'cpu_rate', 2, 2);
 INSERT INTO `aggregator` VALUES (23, 'CPU占用最小值监测', 'cpu_rate', 3, 1);
 INSERT INTO `aggregator` VALUES (24, 'CPU占用求和监测', 'cpu_rate', 4, 3);
-INSERT INTO `aggregator` VALUES (31, 'CPU与内存', 'cpu_mem', 5, 2);
+INSERT INTO `aggregator` VALUES (31, '磁盘占用最大值监测', 'disk_used', 1, 2);
+INSERT INTO `aggregator` VALUES (32, '磁盘占用平均值监测', 'disk_used', 2, 2);
+INSERT INTO `aggregator` VALUES (33, '磁盘占用最小值监测', 'disk_used', 3, 1);
+INSERT INTO `aggregator` VALUES (34, '磁盘占用求和监测', 'disk_used', 4, 3);
+INSERT INTO `aggregator` VALUES (41, 'CPU与内存', 'cpu_mem', 5, 2);
 
 -- ----------------------------
 -- Table structure for function
